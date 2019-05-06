20a4f15a26e2d126fb34422723b5ed95406b9d20
imsxz <lukelenhard@gmail.com>
2019-05-06 16:44:24 -0400

    lab11

---
+++ Lab11/Movie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Movie
+    {
+        private string title;
+        private string category;
+
+        public string Title
+        {
+            set
+            {
+                title = value;
+            }
+            get
+            {
+                return title;
+            }
+        }
+
+        public string Category
+        {
+            set
+            {
+                category = value;
+            }
+            get
+            {
+                return category;
+            }
+        }
+
+        public Movie(string _title, string _category)
+        {
+            Title = _title;
+            Category = _category;
+        }
+    }
+}
---
+++ Lab11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> movies= new List<Movie>() {new Movie("Happy Gilmore", "Comedy"), new Movie("Star Wars", "Scifi"), new Movie("2001: A Space Odyssey", "Scifi"),
+                new Movie("A Clockwork Orange", "Scifi"), /* it is at this moment i got bored searching movies*/new Movie("Movie 5", "Comedy"), new Movie("Movie 6", "Action"), new Movie("Movie 7", "Action")
+            , new Movie("Movie 8", "Comedy"), new Movie("Movie 9", "Scifi"), new Movie("Movie 10", "Action")};//why 10 q-q
+            string wantedCategory;
+            
+            do
+            {
+                Console.WriteLine("Welcome to the Movie List Application." +//kinda wanna put this line and the one below above the dowhile loop but ehhhhhh
+                    $"\n\nThere are {movies.Count} movies in the list." +
+                    $"\nWhat category are you interested in? (action, comedy, scifi)");//i was thinking about making it print distinct categories from the movies list
+                wantedCategory = Console.ReadLine().ToLower();
+
+                List<Movie> foundMovies = new List<Movie>();
+                foreach(Movie movie in movies)
+                {
+                    if(movie.Category.ToLower() == wantedCategory)//most certainly a better way to go about this for line 34 purposes
+                    {//probably just should have had a bool defaulted at false and set it to true if it finds one
+                        foundMovies.Add(movie);
+                        Console.WriteLine(movie.Title);
+                    }
+                }
+                Console.WriteLine(foundMovies.Any() ? "That is all the movies matching your request.":"No movies could be found with that category.");
+
+                Console.WriteLine("\nDo you want to continue? (y/n)");
+            } while (Console.ReadLine().ToLower() == "y");
+        }
+    }
+}
